23a89dce032d0ba005657cb962c6cecad742b8d4
Brian Chadwick <b.matthew.chadwick@gmail.com>
2022-01-28 12:52:16 -0500

    working script

--- cc-pr-create-merge.sh
+++ cc-pr-create-merge.sh
@@ -1,20 +1,27 @@
 #!/bin/bash -ex
 
+TOKEN=$(date "+%Y%m%d%H%M%S")
+REPO=$(basename $(git remote get-url origin))
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 echo "Creating Pull Request"
 
-TOKEN=$(date "+%Y%m%d%H%M%S")
-
 aws codecommit create-pull-request \
-    --title "Creating Pull Request $TOKEN" \
-    --description "scripted PR creation"
+    --title "Scripted Pull Request Creation $TOKEN" \
+    --description "scripted PR creation" \
     --client-request-token $TOKEN \
-    --targets repositoryName=$1,sourceReference=$2
+    --targets repositoryName=$REPO,sourceReference=$BRANCH
 
-echo "Merging Pull Request"
-
-MY_ARN=$()
-
-aws codecommit list-pull-requests \
+echo "Retrieve Pull Request ID"
+MY_ARN=$(aws sts get-caller-identity | jq -r ".Arn")
+PR_ID=$(aws codecommit list-pull-requests \
     --author-arn $MY_ARN \
-    --pull-request-status OPENED \
-    --repository-name $1
+    --pull-request-status OPEN \
+    --repository-name $REPO \
+    --query 'pullRequestIds[0]' --output text)
+
+echo "Merging PR"
+aws codecommit merge-pull-request-by-fast-forward \
+    --pull-request-id $PR_ID \
+    --repository-name $REPO
+
