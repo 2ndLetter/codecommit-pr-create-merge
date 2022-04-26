f3526866ff4d1814db462c63511b3ce60b8aa5f7
2ndLetter <42384647+2ndLetter@users.noreply.github.com>
2022-04-26 10:44:05 -0400

    Update cc-pr-create-merge.sh
    Made the git comment an inline argument

--- cc-pr-create-merge.sh
+++ cc-pr-create-merge.sh
@@ -1,13 +1,15 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-# This script creates/merges a PR for the current directory's repository
+# Usage:   ./cc-pr-create-merge.sh <commit_message_within_quotes>
+# Example: ./cc-pr-create-merge.sh "This is my commit message"
 
-# Push changes to origin
+# Push changes to origin 
+MSG=$1
 git add .
-git commit -m "pushing changes for testing"
+git commit -m "$MSG"
 git push
 
-# Creates/merges a PR for the current directly's repository
+# Creates/merges a PR for the current directory's repository
 TOKEN=$(date "+%Y%m%d%H%M%S")
 REPO=$(basename $(git remote get-url origin))
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
@@ -17,7 +19,8 @@ aws codecommit create-pull-request \
     --title "Scripted Pull Request Creation $TOKEN" \
     --description "scripted PR creation" \
     --client-request-token $TOKEN \
-    --targets repositoryName=$REPO,sourceReference=$BRANCH
+    --targets repositoryName=$REPO,sourceReference=$BRANCH \
+    > /dev/null
 
 echo "Retrieve Pull Request ID"
 MY_ARN=$(aws sts get-caller-identity | jq -r ".Arn")
@@ -30,6 +33,7 @@ PR_ID=$(aws codecommit list-pull-requests \
 echo "Merging PR"
 aws codecommit merge-pull-request-by-fast-forward \
     --pull-request-id $PR_ID \
-    --repository-name $REPO
+    --repository-name $REPO \
+    > /dev/null
 
 echo "Script completed successfully!"
